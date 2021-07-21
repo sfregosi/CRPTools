871c8553975a035e4d786c84a6ff3ec31a190191
sfregosi <22456126+sfregosi@users.noreply.github.com>
2021-07-21 15:48:04 -0700

    reorganized folder/files
    Merged pgUtils with other misc CRP Tools to have all in one place and in my GitHub folder

---
+++ dasbrDriftToGPX.m
@@ -0,0 +1,8 @@
+% convert a dasbr drift csv to a gpx (for reading on the bridge of the
+% Sette)
+
+
+% Ann's code makes a .csv with the dasbr drift information
+% in the field, they then convert this to a kml for plotting in google
+% earth
+% the crew requested something that could be read in their GPS system 
